4571ce1a4010732c1dc967aa1730cbddd9852d6e
DouglasVDM <douglasvdmerwe@gmail.com>
2023-06-04 12:25:58 +0200

    add words table

--- database.sql
+++ database.sql
@@ -1,30 +1,58 @@
-DROP table IF EXISTs nouns;
-CREATE table nouns (
-noun_id serial PRIMARY key,
-noun_name VARCHAR(55) NOT null
+-- Drop the words table
+DROP table IF EXISTs words;
+-- Create the words table
+CREATE TABLE IF NOT EXISTS words (
+  id SERIAL PRIMARY KEY,
+  word_type VARCHAR(100) NOT NULL,
+  word VARCHAR(100) NOT NULL
 );
 
-DROP table IF EXISTs verbs;
-CREATE table verbs (
-verb_id serial PRIMARY key,
-verb_name VARCHAR(55) NOT null
-);
-
-DROP table IF EXISTs determiners;
-CREATE table determiners (
-determiner_id serial PRIMARY key,
-determiner_name VARCHAR(55) NOT null
-);
-
-DROP table IF EXISTs adjectives;
-CREATE table adjectives (
-adjective_id serial PRIMARY key,
-adjective_name VARCHAR(55) NOT null
-);
+-- Insert sample data into the words table
+INSERT INTO words (word_type, word)
+VALUES
+  ('Noun', 'cat'),
+  ('Noun', 'dog'),
+  ('Noun', 'house'),
+  ('Verb', 'run'),
+  ('Verb', 'jump'),
+  ('Adjective', 'beautiful'),
+  ('Adjective', 'happy'),
+  ('Adjective', 'red'),
+  ('Adverb', 'quickly'),
+  ('Adverb', 'loudly'),
+  ('Pronoun', 'he'),
+  ('Pronoun', 'she'),
+  ('Preposition', 'on'),
+  ('Preposition', 'under'),
+  ('Conjunction', 'and'),
+  ('Conjunction', 'but'),
+  ('Determiner', 'the'),
+  ('Determiner', 'a'),
+  ('Exclamation', 'Wow'),
+  ('Exclamation', 'Oh');
 
 DROP table IF EXISTs sentences;
-CREATE table sentences (
-sentence_id serial PRIMARY key,
-sentence_name VARCHAR(1000) NOT null
+ -- Create the sentences table
+CREATE TABLE IF NOT EXISTS sentences (
+  id SERIAL PRIMARY KEY,
+  sentence TEXT NOT NULL
 );
 
+-- Insert sample data into the sentences table
+INSERT INTO sentences (sentence)
+VALUES
+  ('The cat runs quickly.'),
+  ('She jumped and shouted happily.'),
+  ('The red house is under the tree.'),
+  ('He is a happy dog.'),
+  ('Wow, the beautiful sunset!'),
+  ('Oh, the loud music!'),
+  ('A quick brown fox.'),
+  ('The dog barks loudly.'),
+  ('She dances gracefully.'),
+  ('The cat and the dog play together.'),
+  ('The book is on the table.'),
+  ('He ran but tripped.'),
+  ('I saw a cat and a dog.'),
+  ('Wow, what a beautiful flower!'),
+  ('Oh, I forgot my keys!');
