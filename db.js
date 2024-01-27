81086c26e72218cfa3286434d3e2fb2280e25662
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-01-27 20:02:34 +0200

    To fix: Error retrieving ... no pg_hba.conf entry for host "ip address", user "postgres", database "testdb1", no encryption. Add this line

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const pool = new Pool({
 	user: process.env.POSTGRES_USER,
 	password: process.env.POSTGRES_PASSWORD,
 	port: process.env.POSTGRES_PORT,
-	database: process.env.POSTGRES_DB
+	database: process.env.POSTGRES_DB,
+	ssl:{rejectUnauthorized:false},
 });
 
 module.exports = pool;
